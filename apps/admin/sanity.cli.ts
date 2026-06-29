af26823f956dff30a9a3e5c5a7c74859470b4a73
saisaynoomleng <saileng9723@gmail.com>
2026-06-29 16:56:01 -0400

    admin_author_setup

--- apps/admin/sanity.cli.ts
+++ apps/admin/sanity.cli.ts
@@ -3,12 +3,7 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: env.NEXT_PUBLIC_SANITY_DATASET,
-  },
-  typegen: {
-    path: './src/**/*.{js,ts,jsx,tsx}',
-    schema: './src/sanity/schema.json',
-    generates: './src/sanity/types.ts',
+    projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    dataset: env.NEXT_PUBLIC_SANITY_DATASET!,
   },
 });
--- apps/admin/src/app/authors/page.tsx
+++ apps/admin/src/app/authors/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { Bounded, Button, SectionTitle } from '@street-culture/ui';
+import { JSX } from 'react';
+import AuthorList from './AuthorList';
+import { FaPlusCircle } from 'react-icons/fa';
+import EditAuthor from './EditAuthor';
 
-const AuthorPage = () => {
-  return <div>AuthorPage</div>;
+const AuthorPage = (): JSX.Element => {
+  return (
+    <Bounded isCentered={false} className="space-y-3" size="full">
+      <SectionTitle as="h3" size="sm" label="Authors" />
+
+      <div className="grid grid-cols-[300px_1fr] gap-x-3">
+        <div className="flex flex-col gap-y-3">
+          <Button variant="success" className="self-end">
+            <span>New Author</span>
+            <span>
+              <FaPlusCircle />
+            </span>
+          </Button>
+          <AuthorList />
+        </div>
+
+        <div>
+          <EditAuthor />
+        </div>
+      </div>
+    </Bounded>
+  );
 };
 
 export default AuthorPage;
